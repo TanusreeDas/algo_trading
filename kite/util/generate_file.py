835bda02e1ae7a97ffde7823e03681e2ece5ca77
Tanusree-Das <51433237+TanusreeDas@users.noreply.github.com>
2023-08-25 13:26:03 +0100

    [Maintenance] Code beautification and log improvements - Removed redundant comments for clarity. - Enhanced log messages for better readability. - Added additional log messages to provide more context.
    Refactored the code to improve readability and maintainability.

--- kite/util/generate_file.py
+++ kite/util/generate_file.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 def generate_csv_file(file_name,file_content,column_name=None):
 
-    destination_directory = Path("../data")
+    destination_directory = Path("../data/csv")
     current_time = datetime.now().strftime('%d-%m-%Y')
     new_file_name = destination_directory / (file_name+'_'+current_time+'.csv')
 
